08411d95a82fbae40dd5c491b428db251dfcf45b
Sean <sablair@gmail.com>
2016-02-14 21:56:41 +0800

    Implemented FrequentWordsWithMismatches algorithm

---
+++ CourseraConsole/PatternIdentifier.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CourseraConsole
+{
+    public class PatternIdentifier
+    {
+        private readonly ApproximatePatternMatchingCalculator _apm;
+        private readonly PatternConverter _patternConverter;
+        private readonly NeighborhoodGenerator _ngen;
+
+        public PatternIdentifier()
+        {
+            _apm = new ApproximatePatternMatchingCalculator();
+            _patternConverter = new PatternConverter();
+            _ngen = new NeighborhoodGenerator();    
+        }
+
+        public IEnumerable<string> FrequentWordsWithMismatches(string text, int k, int d)
+        {
+            List<string> frequentPatterns = new List<string>();
+            long arrayLength = (long)Math.Pow(4, k);
+
+            var frequencyArray = new int[arrayLength];
+            var close = new int[arrayLength];
+
+            for (int i = 0; i <= text.Length - k; i++)
+            {
+                var neighborhood = _ngen.Neighbors(text.Substring(i, k), d);
+                foreach (var pattern in neighborhood)
+                {
+                    var posIndex = _patternConverter.PatternToNumber(pattern);
+                    close[posIndex] = 1;
+                }
+            }
+
+            for (int index = 0; index < arrayLength; index++)
+            {
+                if (close[index] == 1)
+                {
+                    var pattern = _patternConverter.NumberToPattern(index, k);
+                    frequencyArray[index] = _apm.ApproximateCount(pattern, text, d);
+                }
+            }
+
+            int maxCount = frequencyArray.Max();
+
+            for (int index = 0; index < arrayLength; index++)
+            {
+                if (frequencyArray[index] == maxCount)
+                {
+                    var pattern = _patternConverter.NumberToPattern(index, k);
+                    frequentPatterns.Add(pattern);
+                }
+            }
+
+            return frequentPatterns;
+        }
+    }
+}
