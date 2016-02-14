64ebcdb4f0000c0dec2429c73f100c3dff851973
sblair <0116@hLi>
2016-02-14 12:55:07 +0800

    Resolution of Minimum Skew problem

---
+++ CourseraConsole/SkewFinder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace CourseraConsole
+{
+    public class SkewFinder
+    {
+        public IEnumerable<int> GetMinimumSkewPoints(string dnaSequence)
+        {
+            var result = new List<int>();
+            var skew = 0;
+            var min = int.MaxValue;
+            for(var index = 0; index < dnaSequence.Length; index++)
+            {
+                var nucleotide = dnaSequence[index];
+                switch (nucleotide)
+                {
+                    case 'G':
+                        skew++;
+                        break;
+                    case 'C':
+                        skew--;
+                        break;
+                    default:
+                        break;
+                }
+
+                if (skew <= min)
+                {
+                    if (skew < min)
+                    {
+                        result.Clear();
+                    }
+                    min = skew;
+                    result.Add(index + 1);
+                }
+            }
+            return result;
+        }
+    }
+}
