6c140eb46d68e03921b0b96851cfa9102537f0b7
Sean <sablair@gmail.com>
2016-02-14 14:56:57 +0800

    Resolution of Approximate Pattern Matching problem

---
+++ CourseraConsole/ApproximatePatternMatchingCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace CourseraConsole
+{
+    public class ApproximatePatternMatchingCalculator
+    {
+        private readonly HammingDistanceCalculator _hdc;
+        public ApproximatePatternMatchingCalculator()
+        {
+            _hdc = new HammingDistanceCalculator();
+        }
+
+        public IEnumerable<int> MakeApproximations(string pattern, string text, int mismatches)
+        {
+            var values = new List<int>();
+            for (int index = 0; index <= text.Length - pattern.Length; index++)
+            {
+                if (_hdc.CalculateDistance(pattern, text.Substring(index, pattern.Length)) <= mismatches)
+                {
+                    values.Add(index);
+                }
+            }
+            return values;
+        }
+    }
+}
