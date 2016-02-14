dd80557f129b767ae456de21abc54525d3b03ef8
Sean <sablair@gmail.com>
2016-02-14 14:25:05 +0800

    Resolution of Hamming Distance Calculator

---
+++ CourseraConsole/HammingDistanceCalculator.cs
@@ -0,0 +1,19 @@
+﻿namespace CourseraConsole
+{
+    public class HammingDistanceCalculator
+    {
+        public int CalculateDistance(string sequence1, string sequence2)
+        {
+            int distance = 0;
+
+            for (int index = 0; index < sequence1.Length; index++)
+            {
+                if (sequence1[index] != sequence2[index])
+                {
+                    distance++;
+                }
+            }
+            return distance;
+        }
+    }
+}
